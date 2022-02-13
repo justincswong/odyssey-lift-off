e9e2ba3391d0c5061584023fbcd2f246b94f363d
DESKTOP-274R2DT\Justin <justinc.s.wong@gmail.com>
2022-02-12 23:06:15 -0800

    added new track page

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -4,11 +4,18 @@ const resolvers = {
         // the homepage grid of our web client
         tracksForHome: (_, __, { dataSources }) => {
             return dataSources.trackAPI.getTracksForHome();
+        },
+        // returns a single track by ID
+        track: (_, { id: trackId }, { dataSources }) => {
+            return dataSources.trackAPI.getTrack(trackId);
         }
     },
     Track: {
         author: ({ authorId }, _, { dataSources }) => {
             return dataSources.trackAPI.getAuthor(authorId);
+        },
+        modules: ({ id: trackId }, _, { dataSources }) => {
+            return dataSources.trackAPI.getTrackModules(trackId);
         }
     }
 };
--- server/src/schema.js
+++ server/src/schema.js
@@ -11,6 +11,12 @@ type Track {
     length: Int
     "The number of modules the track contains"
     modulesCount: Int
+    "The track's complete description, can be in MD format"
+    description: String
+    "The number of times a track has been viewed"
+    numberOfViews: Int
+    "The track's complete array of modules"
+    modules: [Module!]!
 }
 
 "Author of a complete Track or a Module"
@@ -20,9 +26,17 @@ type Author {
     photo: String
 }
 
+"A Module is a single unit of teaching. Multiple Modules compose a Track"
+type Module {
+    id: ID!
+    title: String!
+    length: Int
+}
+
 type Query {
     "Get tracks array for homepage grid"
     tracksForHome: [Track!]!
+    track(id: ID!): Track
 }
 `;
 
